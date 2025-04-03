5c6170f7129dcc0e4d436142497d4d3b5d8bba65
Sheryl Deakin <sheryldeakin@Sheryls-MacBook-Pro.local>
2025-04-02 21:59:17 -0400

    fixed csp solving to use crossword object and pattern matching

--- clue_solving/letter_pattern_matching.py
+++ clue_solving/letter_pattern_matching.py
@@ -4,13 +4,28 @@ from nltk.corpus import words
 # Load English words
 word_list = set(words.words())  
 
+# Convert all words to lowercase
+word_list = set(word.lower() for word in words.words())
+
 # Finds words that match a given crossword pattern.
 # A regex pattern (e.g., "c.t" for words like 'cat' and 'cut')
 # returns a list of matching words.
 def find_words_by_pattern(pattern):
-
-    regex = re.compile(pattern)
+    pattern = pattern.lower()
+    regex = re.compile("^" + pattern.replace(".", "[a-z]") + "$")
     return [word for word in word_list if regex.fullmatch(word)]
+
+
+# print("tab" in word_list)
+# print("broth" in word_list)
+# print("jazzy" in word_list)
+# print("tiara" in word_list)
+# print("blitz" in word_list)
+# print("shy" in word_list)
+
+print(len(word_list))
+
+
 
 ######################################################################################################
 # Example Usage
